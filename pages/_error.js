9a9a25f91d8e17dcbfe07d3e0d328295bd0f1954
Danila Stavrov <dan.stavrov@gmail.com>
2020-02-27 16:16:57 +0300

    First commit. Inital configuration: - Inited Redux - Inited Dynamic Post Page scheleton - Inited Dynamic Category Page scheleton - Inited Theme Provider - Added Day and Night Themes

---
+++ pages/_error.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Error extends React.Component {
+    static async getInitialProps({ res, err }) {
+        const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+        return { statusCode };
+    }
+    render() { 
+        return ( 
+            <p>
+                {statusCode
+                    ? `An error ${statusCode} occurred on server`
+                    : 'An error occurred on client'}
+            </p>
+        );
+    }
+}
+ 
+export default Error;
+
+
