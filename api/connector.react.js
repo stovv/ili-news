2fe439218d8aa1cd37220935ff4bb6eacb98c8d8
Danila Stavrov <dan.stavrov@gmail.com>
2020-03-01 05:27:57 +0300

    Fix backend url on Prod

--- api/connector.react.js
+++ api/connector.react.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.BACKEND_URL || 'localhost:1337'
+    baseURL: process.env.BACKEND_URL || 'http://localhost:1337'
 });
 
 export default api;
