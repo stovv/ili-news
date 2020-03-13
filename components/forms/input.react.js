0b0e4b44f2366e09ff1a99cd03c9a1b5a369aca0
Daniel Stovv <dan.stavrov@gmail.com>
2020-03-13 17:20:17 +0000

    :sparkles: Beautify Editor, ready to save editor data

---
+++ components/forms/input.react.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const WhiteListName = styled.input`
+    margin: 1em 0 0 0;
+    padding: 0 0 0 4em;
+    line-height: 1.5em;
+    outline: none;
+    font-size: 3rem;
+    font-weight: 500;
+    width: 100%;
+    border: none;
+    :placeholder{
+        -webkit-text-fill-color: rgba(55, 53, 47, 0.2);
+    }
+`;
+
+export {WhiteListName}
