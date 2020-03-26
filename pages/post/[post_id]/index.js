7107e1f6e1c33524b5f00cc629e76b6b7e930b8f
Danila Stavrov <dan.stavrov@gmail.com>
2020-03-26 04:33:34 +0300

    :sparkles: Added generation of post ( blocks->jsx ) :sparkles: Added fetching emded url :mag: Improve SEO metatags for Posts :recycle: Move global style to style.css

--- pages/post/[post_id]/index.js
+++ pages/post/[post_id]/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { withRouter } from 'next/router';
 import Error from 'next/error';
+import Head from 'next/head';
 import { connect } from "react-redux";
 import {getPost} from '../../../api';
+import generate_jsx from '../../../tools/generator.react';
+import {BACKEND_URL, SITE_URL} from '../../../tools/constants';
+
 
 export const config = { amp: 'hybrid' };
 
@@ -18,17 +22,58 @@ class Post extends React.Component{
                 console.log(reason);
             });
 
-        return {post : current_post, amp: amp}
+        return {
+            error: current_post ? null : 404,
+            created_at: current_post.publication_date ? current_post.publication_date : current_post.created_at,
+            title : current_post.title,
+            cover: `${BACKEND_URL}/${current_post.cover.url}`,
+            current_url: `${SITE_URL}/post/${post_id}`,
+            description: current_post.description,
+            blocks: current_post.blocks.blocks,
+            meta_tags: current_post.meta_tags,
+            authors: current_post.users ? current_post.users : [],
+            amp: amp
+        }
     }
 
     render(){
-        const {post, amp} = this.props;
-        if (this.props.post === null){
-            return <Error statusCode={404}/>;
+        const {blocks, amp, error} = this.props;
+        
+        if (error !== null){
+            return <Error statusCode={error}/>;
         }
         // TODO AMP Checking
         // TODO Use this.props.user for head component
-        return (<p>{post.title} {amp}</p>);
+
+        return (
+            <>
+            <Head>
+                <title>{this.props.title}</title>
+                <meta name="title" content={this.props.title}/>
+                <meta name="description" content={this.props.description}/>
+                <meta property="article:published_time" content={this.props.created_at}/>
+                {/*TODO: Add author url*/}
+                {this.props.authors.map(item=><meta property="article:author" content={item.id}/>)}
+                {this.props.meta_tags.map((item,index)=><React.Fragment key={index}><meta property="article:tag" content={item.tag}/></React.Fragment>)}
+
+                <meta property="og:locale" content="ru_RU"/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content={this.props.current_url}/>
+                <meta property="og:title" content={this.props.title}/>
+                <meta property="og:description" content={this.props.description}/>
+                <meta property="og:image" content={this.props.cover}/>
+                
+                <meta property="og:site_name" content="Молодежный журнал ИЛИ"/>
+
+                <meta property="twitter:card" content="summary_large_image"/>
+                <meta property="twitter:url" content={this.props.current_url}/>
+                <meta property="twitter:title" content={this.props.title}/>
+                <meta property="twitter:description" content={this.props.description}/>
+                <meta property="twitter:image" content={this.props.cover}/>
+            </Head>
+            {generate_jsx(blocks)}
+            </>
+        );
     }
 }
 
